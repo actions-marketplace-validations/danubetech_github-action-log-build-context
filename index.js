4d958872dbbd16ca0d2f228818043c44ba2f4f01
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2021-08-19 15:53:27 +0200

    feat: log specific env variables

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-for (const [key, value] of Object.entries(process.env).sort()) {
+let githubEnv = {};
+let runnerEnv = {};
+let javaEnv = {};
+let globalEnv = {};
+
+for (const [key, value] of Object.entries(process.env)) {
+
+    if (key.startsWith('GITHUB_')) {
+        githubEnv = {...githubEnv, key: value }
+    }
+
+    if (key.startsWith('RUNNER_')) {
+        runnerEnv = {...runnerEnv, key: value }
+    }
+
+    if (key.startsWith('JAVA_')) {
+        javaEnv = {...javaEnv, key: value }
+    }
+
+    if (key.startsWith('GLOBAL_')) {
+        globalEnv = {...globalEnv, key: value }
+    }
+}
+
+console.log('### GLOBAL environment variables set in workflow\n')
+for (const [key, value] of Object.entries(globalEnv)) {
+    console.log(`${key}: ${value}`);
+}
+
+console.log('\n\n### GITHUB environment variables set by Github\n')
+for (const [key, value] of Object.entries(githubEnv)) {
+    console.log(`${key}: ${value}`);
+}
+
+console.log('\n\n### RUNNER environment variables set by Github\n')
+for (const [key, value] of Object.entries(runnerEnv)) {
+    console.log(`${key}: ${value}`);
+}
+
+console.log('\n\n### JAVA environment variables set by Github\n')
+for (const [key, value] of Object.entries(javaEnv)) {
     console.log(`${key}: ${value}`);
 }
